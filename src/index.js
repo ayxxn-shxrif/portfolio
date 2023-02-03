4ae1e6e5b034bc9d1934be917fa7b12fae6c62c9
ayxxn-shxrif <ayaansharif42@gmail.com>
2023-02-03 06:05:08 +0530

    cool favicon

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {DarkModeProvider} from "./contexts/DarkModeContext.js"
+import {LoadingProvider} from './contexts/LoadingContext.js'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <LoadingProvider>
+    <DarkModeProvider>
     <App />
+    </DarkModeProvider>
+    </LoadingProvider>
   </React.StrictMode>
 );
 
